10ddde03d26f7aebebc43979e80edd8f06372bd3
Slash-atello <brad@4real.live>
2018-09-09 00:34:30 +1000

    Krang TF automation started

--- resources/tf/trtl_daemon.tf
+++ resources/tf/trtl_daemon.tf
@@ -5,14 +5,14 @@ resource "digitalocean_ssh_key" "do_ssh_keys" {
 }
 
 # Create a TRTL daemon server - Start with one then figure out how to increment the name and use the same info - also use more variables for changing values in furture
-resource "digitalocean_droplet" "tdm1" {
-  image = "ubuntu-16-04-x64"
-  name = "tdm1"
+resource "digitalocean_droplet" "trtldmn1" {
+  image = "docker-16-04"
+  name = "trtldmn1"
   region = "nyc1"
   size = "512mb"
   ssh_keys = ["43:68:ff:14:66:20:d0:e3:cd:89:09:a2:51:71:98:74"]
   provisioner "remote-exec" {
-    inline = [ "apt-get -y update && apt-get install -y python-minimal" ]
+    inline = [ "apt-get -y update && apt-get install -y python-minimal python-pip && pip install 'docker-py>=1.7.0' 'docker-compose>=1.7.0'" ]
     connection {
       type = "ssh"
       user = "root"
@@ -22,6 +22,6 @@ resource "digitalocean_droplet" "tdm1" {
 }
 
 # OUTPUT THE IP ADDRESS FOR ANSIBLE TO WORK ON 
-# output "address_web" { 
-#  value = "${digitalocean_droplet.web.ipv4_address}"
-# }
+output "address_web" { 
+  value = "${digitalocean_droplet.trtldmn1.ipv4_address}"
+  }
--- resources/tf/variables.tf
+++ resources/tf/variables.tf
@@ -1,12 +1,12 @@
 variable "do_token" {
 }
 variable "do_region" {
-  default = "nyc1"
+  default = "sin1"
 }
 variable "do_image" {
   default = "ubuntu-16-04-x64"
 }
 variable "do_username" {
-  default = "cjones"
+  default = "slasha"
 }
 
